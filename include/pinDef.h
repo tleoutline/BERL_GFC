0a49c71eeb5552c0b131296912c81972e4047b2d
tleoutline <tleoutline@gmail.com>
2023-02-02 16:51:03 +0700

    Change output to external DAC

--- include/pinDef.h
+++ include/pinDef.h
@@ -1,9 +1,9 @@
-#define DAC_PIN     A0
 #define ENCODER_SW  A1
 #define ENCODER_A   A2
 #define ENCODER_B   A3
-#define PWM_PIN     A10
 
-#define FAST_ROTATE_COUNT       3
-#define FAST_ROTATE_AMOUNT      50
-#define DEBOUNCE_INTERVAL_MS    300
+
+#define ROTATE_AMOUNT               4
+#define FAST_ROTATE_AMOUNT          160
+#define FAST_ROTATE_INTERVAL_MS     50
+#define DEBOUNCE_INTERVAL_MS        300
--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,20 @@
 #include <Arduino.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
-#include <Adafruit_ADS1X15.h>
 #include <Adafruit_MCP4725.h>
 #include <Encoder.h>
 #include <Wire.h>
 
 #include "pinDef.h"
 
-#define DAC_RESOLUTION 4095    // 12-bits DAC resolution
+#define DAC_RESOLUTION  4095    // 12-bits DAC resolution
+#define REF_VOLTAGE     5.0
 
 Adafruit_SSD1306 display = Adafruit_SSD1306(128, 64, &Wire);
 Adafruit_MCP4725 dac;
 Encoder encoder(ENCODER_A, ENCODER_B);
-uint16_t dacVal = DAC_RESOLUTION / 2;     // write half the reference voltage on start
-const float refVoltage = 5.0;
-float voltage = refVoltage / 2;
+int16_t dacVal = DAC_RESOLUTION / 2;     // write half the reference voltage on start
+float voltage = REF_VOLTAGE / 2;
 bool encoderSWFlag = false;
 char msgBuffer[64];
 
@@ -28,19 +27,6 @@ void encoderPressed() {
   }
 }
 
-void encoder2Val(long encoderChanges) {
-  long changes = 0;
-  if (encoderChanges > 0) {
-    changes = (encoderChanges > FAST_ROTATE_COUNT ? FAST_ROTATE_AMOUNT : 10) * encoderChanges;
-    dacVal = min(DAC_RESOLUTION, dacVal + changes);
-  }
-  else if (encoderChanges < 0) {
-    changes = (-encoderChanges > FAST_ROTATE_COUNT ? -FAST_ROTATE_AMOUNT : -10) * -encoderChanges;
-    dacVal = max(0L, dacVal + changes);
-  }
-  voltage = refVoltage * dacVal / DAC_RESOLUTION;
-  return;
-}
 
 void print2Display(String msg) {
   display.clearDisplay();
@@ -50,7 +36,8 @@ void print2Display(String msg) {
 }
 
 void setup() {
-  dac.begin(0x60); // 0x60 to 0x63
+  // Serial.begin(9600);
+  dac.begin(0x61); // 0x60 to 0x63
   pinMode(LED_BUILTIN, OUTPUT);
   pinMode(ENCODER_SW, INPUT_PULLUP);
 
@@ -63,34 +50,49 @@ void setup() {
   display.setTextSize(2);
   display.setTextColor(SSD1306_WHITE);
   display.setCursor(0, 0);
-  sprintf(msgBuffer, "Current Voltage: \n%.2f V", voltage);
+  sprintf(msgBuffer, "Current\nVoltage: \n%.3f V", voltage);
   print2Display(msgBuffer);
 
   attachInterrupt(ENCODER_SW, encoderPressed, FALLING);
 }
 
 void loop() {
-  static long encoderPos = 0;
-
   if (encoderSWFlag) {
     encoderSWFlag = false;
-    sprintf(msgBuffer, "Set new Voltage:\n%.2f V", voltage);
+    uint32_t encoderPos = encoder.read();
+    unsigned long rotateStamp, lastRotateStamp = millis();
+    sprintf(msgBuffer, "Set new\nVoltage:\n%.3f V", voltage);
     print2Display(msgBuffer);
 
     while (true) {
       if (encoderSWFlag) { encoderSWFlag = false; break; }
-      long newPos = encoder.read();
+      uint32_t newPos = encoder.read();
+      int16_t changes;
+      static uint8_t fastRotateCount = 0;
+
       if (newPos != encoderPos) {
-        encoder2Val(newPos - encoderPos);
+        rotateStamp = millis();
+
+        if (rotateStamp - lastRotateStamp <= FAST_ROTATE_INTERVAL_MS) { fastRotateCount++; }
+        else { fastRotateCount = 0; }
+        if (fastRotateCount > 2) { changes = (newPos > encoderPos) ? FAST_ROTATE_AMOUNT : -FAST_ROTATE_AMOUNT; }
+        else { changes = (newPos > encoderPos) ? ROTATE_AMOUNT : -ROTATE_AMOUNT; }
+        lastRotateStamp = rotateStamp;
+
+        dacVal += changes;
+        dacVal = max(0, dacVal);
+        dacVal = min(DAC_RESOLUTION, dacVal);
+        voltage = dacVal * REF_VOLTAGE / DAC_RESOLUTION;
+
         encoderPos = newPos;
-        sprintf(msgBuffer, "Set new Voltage:\n%.2f V", voltage);
+        sprintf(msgBuffer, "Set new\nVoltage:\n%.3f V", voltage);
         print2Display(msgBuffer);
         dac.setVoltage(dacVal, false);
       }
-      delay(100);
+      delay(1);
     }
 
-    sprintf(msgBuffer, "Current Voltage:\n%.2f V", voltage);
+    sprintf(msgBuffer, "Current\nVoltage:\n%.3f V", voltage);
     print2Display(msgBuffer);
   }
 }
